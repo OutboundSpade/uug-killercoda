8dc10f78f656eb01f364ef5bdeddae40136e397c
Stingray <44787842+OutboundSpade@users.noreply.github.com>
2023-11-08 17:48:12 -0500

    Some more changes

--- Docker/assets/nodejs/index.js
+++ Docker/assets/nodejs/index.js
@@ -5,3 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
 	res.send("Hello World");
 });
+
+app.listen(8080, () => {
+	console.log("Server is listening on port 8080");
+});
--- Docker/assets/portainer/start.sh
+++ Docker/assets/portainer/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce:latest
+docker run --rm -d -p 80:9000 --name portainer -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce:latest
